2749b0c3d5792dfce8f2916049306f6671438d16
IoanaMarin1106 <marinvioana@gmail.com>
2022-05-26 16:12:21 +0300

    first commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/ClientThread.java
@@ -0,0 +1,79 @@
+package ro.pub.cs.systems.eim.practicaltest02.network;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.general.Utilities;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private String clientInfo;
+    private TextView resultTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String clientInfo, TextView resultTextView) {
+        this.address = address;
+        this.port = port;
+        this.resultTextView = resultTextView;
+        this.clientInfo = clientInfo;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+
+            // TODO: clientul trimite catre server ce date vrea sa obtina
+            // hint: printWriter.println(date)
+            printWriter.println(clientInfo);
+            printWriter.flush();
+
+            String information;
+            while ((information = bufferedReader.readLine()) != null) {
+                final String finalizedInformation = information;
+                resultTextView.post(new Runnable() {
+                   @Override
+                    public void run() {
+                       resultTextView.setText(finalizedInformation);
+                   }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
